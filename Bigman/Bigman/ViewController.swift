d56bdf4e4bc619b2c0f8bd1331ce688d83e8ef40
yanglong <18205186136@163.com>
2018-11-20 15:18:48 +0800

    initialize

---
+++ Bigman/Bigman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bigman
+//
+//  Created by yangl on 2018/11/20.
+//  Copyright © 2018 yangl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
